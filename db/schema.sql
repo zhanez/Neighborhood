3a63a8be6bf1d9f94d393403ab62b8e0c33bbc02
Truc Nguyen <trucn0215@gmail.com>
2021-01-20 19:36:20 -0800

    installed passport and mysql2

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS neighborhood_db;
+CREATE DATABASE neighborhood_db;
+
+-- USE neighborhood_db;
+
+-- CREATE TABLE neighberhood(
+--     id INT AUTO_INCREMENT NOT NULL,
+--     burger_name VARCHAR (30) NOT NULL,
+--     devoured BOOLEAN DEFAULT false,
+--     PRIMARY KEY (id)
+-- );
