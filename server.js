2d6de4ed2d32711896b26d384b767ba8e81ee8cd
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-01-20 22:31:37 -0800

    updated server.js, installed express-session

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Requiring necessary npm packages
 var express = require("express");
-// var session = require("express-session");
+var session = require("express-session");
+
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
@@ -13,18 +14,18 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-// We need to use sessions to keep track of our user's login status
-// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
 
-// Requiring our routes
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Requiring routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon successs
 db.sequelize.sync({force: true}).then(function () {
     app.listen(PORT, function () {
-        console.log("==> 🌎  Listening on port %s. Visit http://localhost:" + PORT + "in your browser!");
+        console.log("Server listening on: http://localhost:" + PORT);
     });
 });
