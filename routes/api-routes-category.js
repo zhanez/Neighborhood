26f9eb40599e71cc58c65153d79277ded06e7033
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-01-21 20:12:30 -0800

    wrote pseudocode logic for category table api routes

---
+++ routes/api-routes-category.js
@@ -0,0 +1,7 @@
+const db = require("../models");
+
+//post route to the category table after user clicks "add category" button
+
+//get route to display ALL categorys in "existing category" section
+
+//delete route to delete a category. (would need to add a CASCADE delete to category table)
