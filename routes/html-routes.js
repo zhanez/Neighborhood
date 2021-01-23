bc86e5cb3b8e864f95fe753bc5776124c4f29839
Truc Nguyen <trucn0215@gmail.com>
2021-01-23 09:45:19 -0800

    add code to route to pages and API but does not work yet

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,34 @@
+// Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
-
+// Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated.js");
 
-module.exports = function(app){
-    app.get("/", function(req, res){
-        if(req.table){
-            res.redirect("/grouptemplate");
-        }
-        res.sendfile(path.join(__dirname, "../public/login.html"));
-    })
+module.exports = function (app) {
+
+    // route to homepage = login page
+    app.get("/", function (req, res) {
+        // if (req.user) {
+        //     res.redirect("/members");
+        // }
+        res.sendFile(path.join(__dirname, "../public/login.html"));
+    });
+
+    //route to signup page
+    app.get("/signup", function (req, res) {
+        // If the user already has an account send them to the members page
+        // if (req.user) {
+        //     res.redirect("/grouptemplate");
+        // }
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
+
+    //route to grouptemplate page
+    app.get("/grouptemplate", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/grouptemplate.html"));
+    });
+
+    // route to members page
+    app.get("/members", isAuthenticated, function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/members.html"));
+    });
 }
