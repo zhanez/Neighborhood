efe613815b1615b1da3fcbc2cf43d72f328f8f54
Truc Nguyen <trucn0215@gmail.com>
2021-01-22 23:14:04 -0800

    pull down the update

--- models/category.js
+++ models/category.js
@@ -1,14 +1,14 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Category = sequelize.define("Category", {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-});
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
 
-Category.associate = function(models) {
-    Category.belongsToMany(models.User, {through: models.UserCategory});
-  };
+    Category.associate = function (models) {
+        Category.belongsToMany(models.User, { through: models.UserCategory });
+    };
 
-return Category;
+    return Category;
 };
