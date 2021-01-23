1cd8a3a93431cad24907ff21f78a78c4a99dcfdc
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-01-23 13:10:04 -0800

    updated login.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -21,7 +21,7 @@ module.exports = function(app){
     app.get("/login", function (req, res) {
         //If the user already has an account send them to the members page
         if (req.user) {
-            res.redirect("/grouptemplate");
+            res.redirect("/members");
         }
         res.sendFile(path.join(__dirname, "../public/login.html"));
     });
