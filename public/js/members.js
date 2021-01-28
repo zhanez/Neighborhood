c3cc07e0587f181db70ee2ae52efbfefe3a86646
Truc Nguyen <trucn0215@gmail.com>
2021-01-28 00:21:51 -0800

    able to display user info to Profile block. Also changed text color for it for better visual. Signup and login button color as well

--- public/js/members.js
+++ public/js/members.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
   $.get("/api/user").then(data => {
     $("#emailUser").text("Email: " + data.email);
     $("#idUser").attr("data-id", data.id).text("ID: " + data.id);
-    $("#nameUser").text(data.first_name + " " + data.last_name);
+    $("#nameUser").text("Name: " + data.first_name + " " + data.last_name);
     $("#bioUser").text("Bio: " + data.bio);
   });
 
